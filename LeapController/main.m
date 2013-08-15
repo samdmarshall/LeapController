4b152e823a72fe65a80ebb618e2a69ddcc4fdab4
Sam <me@samdmarshall.com>
2013-08-15 15:29:30 -0400

    leap motion controller test

---
+++ LeapController/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  LeapController
+//
+//  Created by Sam on 8/15/13.
+//  Copyright (c) 2013 Sam. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+	@autoreleasepool {
+	    
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	    
+	}
+    return 0;
+}
+
